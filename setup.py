ff213f97ca089837a688b35af5482f3e44294d64
keikhosravi <keikhosravi@wisc.edu>
2023-11-24 11:04:34 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,38 +1,57 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name="hitips",
     version="0.1",
+    author="keikhosravi",
+    author_email="adib.keikhosravi@nih.gov",
+    description="HiTIPS: High-Throughput Image Processing Software for FISH data analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CBIIT/HiTIPS",
     packages=find_packages(),
     install_requires=[
-		    'numpy',
-		    'opencv-python-headless',
-		    'scikit-image',
-		    'scipy',
-		    'Pillow',
-		    'pandas',
-		    'matplotlib',
-		    'btrack',  # Ensure this exists on PyPI
-		    'imageio',
-		    'tifffile',
-		    'aicsimageio',
-		    'deepcell',  # Ensure this exists on PyPI
-		    'scikit-learn',
-		    'hmmlearn',
-		    'PyQt5',
-		    'cellpose',
-		    'tensorflow',
-		    'joblib',
-		    'dask',
-		    'nd2reader',
-		    'imaris_ims_file_reader',
-		    'qimage2ndarray'
-		],
-
+        'numpy',
+        'opencv-python-headless',
+        'scikit-image',
+        'scipy',
+        'Pillow',
+        'pandas',
+        'matplotlib',
+        'btrack',  
+        'imageio',
+        'tifffile',
+        'aicsimageio',
+        'deepcell',  
+        'scikit-learn',
+        'hmmlearn',
+        'PyQt5',
+        'cellpose',
+        'tensorflow',
+        'joblib',
+        'dask',
+        'nd2reader',
+        'imaris_ims_file_reader',
+        'qimage2ndarray',
+        'spatial_efd'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Image Processing',
+    ],
+    python_requires='>=3.7',
     entry_points={
         'console_scripts': [
             'hitips=hitips.HiTIPS:main',
         ],
     },
+    license="MIT",
+    keywords="high-throughput imaging FISH analysis cell segmentation signal quantification",
 )
 
