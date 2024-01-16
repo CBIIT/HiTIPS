a54613abe39d425a8370f865275b8e68012052cf
keikhosravi <keikhosravi@wisc.edu>
2024-01-15 23:25:49 -0500

    Add files via upload

--- hitips/Analysis.py
+++ hitips/Analysis.py
@@ -448,7 +448,7 @@ class ImageAnalyzer(object):
         intensity thresholding, and Enhanced LOG.
 
         Parameters:
-            - **kwargs (dict): Keyword arguments containing parameters for the spot detection process.**
+            - kwargs (dict): Keyword arguments containing parameters for the spot detection process.
             The function expects the following key-value pairs:
             - 'input_image_raw' (numpy.ndarray): Raw input image for spot detection.
             - 'nuclei_image' (numpy.ndarray): Image of nuclei, used in preprocessing.
