1358541ecd9c798593cda6dd7ebac40e22253066
keikhosravi <keikhosravi@wisc.edu>
2024-01-22 16:11:43 -0500

    Add files via upload

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.20',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.22',
     LANGUAGE: 'python',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
